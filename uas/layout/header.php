281de251b311366bfbe66ffe1639e2e1761317d7
Asep Dwi Saputra <127648406+asepdwisaputra@users.noreply.github.com>
2023-05-21 19:44:40 +0700

    Add files via upload

---
+++ uas/layout/header.php
@@ -0,0 +1,39 @@
+<?php
+
+include'config/app.php';
+
+
+?>
+
+
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+    <title>UAS</title>
+  </head>
+  <body>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container">
+                <a class="navbar-brand" href="#">Asep</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="#">Barang</a>
+                    <a class="nav-link" href="#">Mahasiswa</a>
+                    <a class="nav-link" href="#">Modal</a>
+                </div>
+                </div>
+            </div>
+        </nav>
+    </div>
